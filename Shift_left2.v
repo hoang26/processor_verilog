2eed73e5eea57ec3bca519968dc8420556a81063
Eric <eric.cc.su@gmail.com>
2015-11-24 12:25:40 -0500

    Shift left 2 added

---
+++ Shift_left2.v
@@ -0,0 +1,12 @@
+module Shift_left2(in, out, clk);
+
+    input wire [31:0] in;
+    input wire clk;
+
+    output reg [31:0] out;
+
+    always@(posedge clk)
+    begin
+        out <= in << 2;
+    end
+endmodule
