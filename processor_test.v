3a2865f2af569b800a6f8282bf07ed8f72ed62b9
Eric <eric.cc.su@gmail.com>
2015-12-10 14:50:42 -0500

    Test processor step 2

--- processor_test.v
+++ processor_test.v
@@ -6,6 +6,9 @@ module processor_test (input wire clk,
 wire [31:0] pc_in;
 wire [31:0] pc_out;
 
+assign add0_in1 = 32'd4;
+
+//************************ STEP 1 - PC ************************************
 pc pc0(
 	.in (pc_in), //32 bits
 	.clk,
@@ -13,3 +16,13 @@ pc pc0(
 	.en (pc_enable),
 	.out (pc_out) //32 bits
 	);
+
+//******************** STEP 2 - PC + 4 ADDER ********************************
+Adder_32b pc_adder(
+	.clk,
+	.input0 (pc_out),
+	.input1 (add0_in1),
+	.out (pc_plus_four)
+	);
+
+endmodule
--- processor_test_tb.v
+++ processor_test_tb.v
@@ -7,7 +7,7 @@ reg [31:0] in;
 
 wire [31:0] out;
 
-pc DUT(in, clk, rst, en, out);
+processor_test DUT(clk, rst, en);
 
 // Initial Conditions
 initial begin
@@ -21,11 +21,16 @@ always #5 clk=~clk;
 
 // Input Waveform
 initial begin
+
+    //Step 1 - test if PC resets to 0x3000
+    //Step 2 - test if adder does PC+4
     rst=1;
     en=1;
 
     #15
     rst=0;
+
+
 end
 
 endmodule
